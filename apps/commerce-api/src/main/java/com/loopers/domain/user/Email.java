d446d6e47a60657ae304542e065b5fa59cd30929
Seyoung Hong <maiorem00@gmail.com>
2025-07-13 17:47:12 +0900

    feat: 유저 도메인 생성

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/Email.java
@@ -0,0 +1,49 @@
+package com.loopers.domain.user;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+@Embeddable
+public class Email{
+
+    //xx@yy.zz 형식
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String value;
+
+    protected Email() {}
+
+    public Email(String value) {
+        if (value == null || value.isEmpty()) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "이메일은 비어있을 수 없습니다.");
+        }
+        if (!EMAIL_PATTERN.matcher(value).matches()){
+            throw new CoreException(ErrorType.BAD_REQUEST, "이메일 형식에 맞지 않습니다.");
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equals(value, email.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ apps/commerce-api/src/main/java/com/loopers/domain/user/UserId.java
@@ -0,0 +1,49 @@
+package com.loopers.domain.user;
+
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+@Embeddable
+public class UserId {
+
+    // 영문 및 숫자 (4~10자 이내)
+    private static final String USER_ID_REGEX = "^[a-zA-Z0-9]{4,10}$";
+    private static final Pattern USER_ID_PATTERN = Pattern.compile(USER_ID_REGEX);
+
+    @Column(name = "user_id", nullable = false, unique = true, length = 10)
+    private String value;
+
+    protected UserId() {
+    }
+
+    public UserId(String value) {
+        if (value == null || value.isBlank()) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "아이디는 비어있을 수 없습니다.");
+        }
+        if (!USER_ID_PATTERN.matcher(value).matches()) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "아이디는 10자 이하의 영문 소문자와 숫자로만 구성되어야 합니다.");
+        }
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserId userId = (UserId) o;
+        return Objects.equals(value, userId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
