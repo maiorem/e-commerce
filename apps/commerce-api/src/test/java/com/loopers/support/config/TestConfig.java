015086e86a83432a76467bdbf5d479bc0537b1f9
Seyoung Hong <maiorem00@gmail.com>
2025-07-17 08:51:57 +0900

    debug: @SpyBean deprecated 해결

--- apps/commerce-api/src/test/java/com/loopers/support/config/TestConfig.java
+++ apps/commerce-api/src/test/java/com/loopers/support/config/TestConfig.java
@@ -2,7 +2,6 @@ package com.loopers.support.config;
 
 
 import com.loopers.domain.user.UserRepository;
-import com.loopers.domain.user.UserService;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
@@ -10,12 +9,6 @@ import static org.mockito.Mockito.spy;
 
 @TestConfiguration
 public class TestConfig {
-
-    // Mockito.spy()를 사용해 스파이 객체를 직접 생성
-    @Bean
-    public UserService userService(UserService realUserService) {
-        return spy(realUserService);
-    }
 
     @Bean
     public UserRepository userRepository(UserRepository realUserRepository) {
